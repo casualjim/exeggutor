aeec24141ce70e0ddfa5f2e0298d956c35056592
Ivan Porto Carrero <ivan@flanders.co.nz>
2014-06-18 20:51:40 -0700

    add a minimal makefile and the most minimal example of a docker container for deployment

--- samples/helloworld/main.go
+++ samples/helloworld/main.go
@@ -5,27 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"strconv"
 )
 
 func main() {
 	http.HandleFunc("/", helloWold)
-	addr := fmt.Sprintf(":%v", readPort(3000))
+	addr := fmt.Sprintf(":%v", 3000)
 	log.Println("Starting webserver at", addr)
 	http.ListenAndServe(addr, nil)
-}
-
-func readPort(def int) int {
-	port := os.Getenv("PORT")
-	if port != "" {
-		p, err := strconv.Atoi(port)
-		if err != nil {
-			log.Fatalf("The value of the port environment variable is %v which is not convertible to int", port)
-		}
-		return p
-	}
-	return def
 }
 
 func helloWold(w http.ResponseWriter, r *http.Request) {
